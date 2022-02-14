e7abf0b41cda667a089d3b91863540fed8aa05d2
hyjoong <maplesosser@naver.com>
2022-02-14 21:17:10 +0900

    feat: useCategoryList 커스텀 훅 구현

---
+++ hooks/useCategoryList.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { CategoryData } from "./../types/index";
+import { getCategory } from "api/api";
+import useRequest from "./useRequest";
+
+const useCategoryList = () => {
+  const [results, setResults] = useState<CategoryData[]>();
+
+  const { loading, responseOK } = useRequest(async () => {
+    const response = await getCategory();
+    setResults(response.data);
+  });
+
+  return { results, loading, responseOK };
+};
+
+export default useCategoryList;
