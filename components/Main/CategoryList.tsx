b4f6c10f435647bf99372b54f629eaaf0a90159a
hyjoong <maplesosser@naver.com>
2022-02-15 10:33:35 +0900

    feat: main page 데이터 getStaticProps로 받아오게 수정

--- components/Main/CategoryList.tsx
+++ components/Main/CategoryList.tsx
@@ -1,28 +1,16 @@
 import React from "react";
-import styled from "styled-components";
-import useCategoryList from "hooks/useCategoryList";
 import GridLayout from "components/Layout/GridLayout";
 import CategoryItem from "components/common/CategoryItem";
+import { CategoryData } from "types";
 
-const CategoryList = () => {
-  const { results, loading, responseOK } = useCategoryList();
+interface IProps {
+  categories: CategoryData[];
+}
 
-  if (!results) {
-    return <>Loading</>;
-  }
-
-  if (loading) {
-    return <div>로딩중... </div>;
-  }
-
-  if (!loading && !responseOK) {
-    return <div>Not Found</div>;
-  }
-
-  console.log(results);
+const CategoryList = ({ categories }: IProps) => {
   return (
     <GridLayout>
-      {results.map((item, index) => (
+      {categories.map((item, index) => (
         <CategoryItem
           key={index}
           thumbnail={item.imageUrl}
@@ -32,15 +20,5 @@ const CategoryList = () => {
     </GridLayout>
   );
 };
-
-const ItemImg = styled.img`
-  width: 40px;
-`;
-
-const ItemName = styled.div`
-  margin-top: 12px;
-  font-size: 0.7rem;
-  font-weight: 600;
-`;
 
 export default CategoryList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,34 @@
-const Index = () => {
-  return <div></div>;
+import styled from "styled-components";
+import CategoryList from "components/Main/CategoryList";
+import { getCategory } from "api/api";
+import { CategoryData } from "types";
+
+interface IProps {
+  categories: CategoryData[];
+}
+
+const Index = ({ categories }: IProps) => {
+  console.log(categories);
+  return (
+    <MainWrapper>
+      <CategoryList categories={categories} />
+    </MainWrapper>
+  );
+};
+
+const MainWrapper = styled.div`
+  max-width: 375px;
+  background-color: #f1f3f4;
+  margin: 0 auto;
+`;
+
+export const getStaticProps = async () => {
+  const data = await getCategory();
+  return {
+    props: {
+      categories: data.conCategory1s,
+    },
+  };
 };
 
 export default Index;
