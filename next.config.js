470ef7a693ccbb029795363f46d68f6e43b416f3
henry <70426440+hyjoong@users.noreply.github.com>
2023-05-13 16:18:11 +0900

    feat: next/image domain 추가 및 minimumCacheTTL 값 수정 - minimumCacheTTL : 86400 -> 2678400

--- next.config.js
+++ next.config.js
@@ -5,8 +5,13 @@ module.exports = {
     appDir: true,
   },
   images: {
-    domains: ["d1dsr05o5i286u.cloudfront.net", "d1tftw9jjs9bx5.cloudfront.net"],
-    minimumCacheTTL: 86400,
+    domains: [
+      "d1dsr05o5i286u.cloudfront.net",
+      "d1tftw9jjs9bx5.cloudfront.net",
+      "d2lfdrnysfmya6.cloudfront.net",
+      "d2na9jaypdrp94.cloudfront.net",
+    ],
+    minimumCacheTTL: 2678400,
     formats: ["image/avif", "image/webp"],
   },
 };
