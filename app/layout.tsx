70238c936024b393e1f88d0ff8dbfddfb56a84a4
henry <70426440+hyjoong@users.noreply.github.com>
2023-07-13 23:27:23 +0900

    feat:  parallel, intercepting routes & modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,20 @@ import styles from "./layout.module.css";
 import { Registry } from "./ui/Registry";
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <html lang="ko">
       <body>
         <Registry>
           <div className={styles.main}>
-            <div className={styles.content}>{children}</div>
+            <div className={styles.content}>
+              {children}
+              {modal}
+            </div>
           </div>
         </Registry>
       </body>
