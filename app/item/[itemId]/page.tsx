53176039cb662406a1eacd0a174f7a489420e6be
henry <70426440+hyjoong@users.noreply.github.com>
2023-07-16 18:47:10 +0900

    refactor: item detail page SEOHeader 제거

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -5,6 +5,15 @@ import Header from "components/common/Header";
 import ItemContainer from "components/Item/ItemContainer";
 type Params = { params: { itemId: string } };
 
+const getItemData = async (itemId: number) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URI}/con-items//${itemId}`,
+    { cache: "no-store" }
+  );
+  const data = await res.json();
+  return data.conItem;
+};
+
 export const generateMetadata = async ({
   params: itemId,
 }: Params): Promise<Metadata> => {
@@ -38,26 +47,11 @@ export const generateMetadata = async ({
   }
 };
 
-const getItemData = async (itemId: number) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URI}/con-items//${itemId}`,
-    { cache: "no-store" }
-  );
-  const data = await res.json();
-  return data.conItem;
-};
-
 export default async function ({ params: { itemId } }: Params) {
   const itemInfo = await getItemData(Number(itemId));
 
   return (
     <>
-      <SEOHeader
-        title={`${itemInfo.name}`}
-        description={`${itemInfo.information}`}
-        imageUrl={`${itemInfo.imageUrl}`}
-        siteUrl={`https://ncnc.vercel.app/items/${itemInfo.itemId}}`}
-      />
       <Header title="" />
       <ItemContainer item={itemInfo}></ItemContainer>
     </>
