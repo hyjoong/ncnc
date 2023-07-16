8415b43bc9881cbfe1d1a6c8ec22a4d2ab4698e4
henry <70426440+hyjoong@users.noreply.github.com>
2023-07-16 18:37:53 +0900

    feat: item detail page meta data

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import type { Metadata } from "next";
 import SEOHeader from "hooks/SEOHeader";
 import Header from "components/common/Header";
 import ItemContainer from "components/Item/ItemContainer";
-
 type Params = { params: { itemId: string } };
+
+export const generateMetadata = async ({
+  params: itemId,
+}: Params): Promise<Metadata> => {
+  try {
+    const itemInfo = await getItemData(Number(itemId));
+
+    return {
+      title: `ncnc | ${itemInfo.name}`,
+      description: itemInfo.information,
+      openGraph: {
+        url: `https://ncnc.vercel.app`,
+        title: `${itemInfo.title}`,
+        siteName: "ncnc",
+        description: itemInfo.information,
+        images: [
+          {
+            url: `${itemInfo.imageUrl}`,
+            width: 80,
+            height: 80,
+            alt: `product picture: ${itemInfo.title}`,
+            type: "image/png",
+          },
+        ],
+      },
+    };
+  } catch (e) {
+    return {
+      title: "ncnc | Not found",
+      description: "The resource you were looking for does not exist",
+    };
+  }
+};
 
 const getItemData = async (itemId: number) => {
   const res = await fetch(
