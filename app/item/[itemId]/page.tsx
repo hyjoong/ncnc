2c5c434b9cc9f7ddbb70a274bd2b9eeaa2bf0f0b
henry <70426440+hyjoong@users.noreply.github.com>
2023-07-16 20:22:01 +0900

    refactor: params: { itemId }, 구조분해 할당

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -15,28 +15,36 @@ const getItemData = async (itemId: number) => {
 };
 
 export const generateMetadata = async ({
-  params: itemId,
+  params: { itemId },
 }: Params): Promise<Metadata> => {
-  const itemInfo = await getItemData(Number(itemId));
-  return {
-    title: `ncnc | ${itemInfo.name}`,
-    description: itemInfo.information,
-    openGraph: {
-      url: `https://ncnc.vercel.app/item/${itemId}`,
-      title: `ncnc | ${itemInfo.title}`,
-      siteName: "ncnc",
+  try {
+    const itemInfo = await getItemData(Number(itemId));
+
+    return {
+      title: `ncnc | ${itemInfo.name}`,
       description: itemInfo.information,
-      images: [
-        {
-          url: `${itemInfo.imageUrl}`,
-          width: 80,
-          height: 80,
-          alt: `product picture: ${itemInfo.title}`,
-          type: "image/png",
-        },
-      ],
-    },
-  };
+      openGraph: {
+        url: `https://ncnc.vercel.app/item/${itemId}`,
+        title: `ncnc | ${itemInfo.name}`,
+        siteName: "ncnc",
+        description: itemInfo.information,
+        images: [
+          {
+            url: `${itemInfo.imageUrl}`,
+            width: 80,
+            height: 80,
+            alt: `product: ${itemInfo.name}`,
+            type: "image/png",
+          },
+        ],
+      },
+    };
+  } catch (e) {
+    return {
+      title: "ncnc | Not found",
+      description: "The resource you were looking for does not exist",
+    };
+  }
 };
 
 export default async function ({ params: { itemId } }: Params) {
