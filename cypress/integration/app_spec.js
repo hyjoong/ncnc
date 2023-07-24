d3d17dc0f9cd93eb4fca2958a44186a64d20d6f8
henry <70426440+hyjoong@users.noreply.github.com>
2023-07-24 22:59:46 +0900

    refactor: cypress logic 수정

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,30 +1,30 @@
 const BASE_URL = "http://localhost:3000/";
 const API_URL = "https://api2.ncnc.app/";
-const BRAND_ID = 1;
-const ITEMLIST_ID = 69;
-const ITEM_ID = 7009;
+const BRAND_ID = 67;
+const ITEMLIST_ID = 63;
+const ITEM_ID = 137;
 
 describe("E2E Test", () => {
   before(() => {
     cy.visit(`${BASE_URL}`);
   });
-  it("페이지에 처음 접속하면  헤더를 볼 수 있다", () => {
-    cy.get(".header").should("be.visible");
+  it("페이지에 처음 접속하면  오늘의 떙처리콘! 글자를 확인할 수 있다", () => {
+    cy.contains("오늘의 떙처리콘!", { timeout: 3000 }).should("be.visible");
   });
 
-  it("페이지에 처음 접속하면 전체 게임 목록을 볼 수 있다.", () => {
+  it("페이지에 처음 접속하면 카테고리 탭을 볼 수 있다.", () => {
     cy.get(".categoryBox", { timeout: 3000 }).should("be.visible");
   });
 
-  it("사용자는 카테고리를 클릭해서 브랜드 목록 페이지로 들어갈 수 있다.", () => {
-    cy.get(".categoryBox").first().click();
+  it("사용자는 카테고리를 클릭해서 브랜드 페이지로 이동할 수 있다.", () => {
+    cy.get(".categoryBox", { timeout: 3000 }).eq(1).click();
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq(`/brand/${BRAND_ID}`);
     });
   });
 
-  it("사용자는 브랜드 목록 페이지에서 브랜드를 선택하면 해당하는 브랜드 상품들을 볼 수 있다..", () => {
-    cy.get(".categoryBox").first().click();
+  it("사용자는 브랜드 페이지에서 특정 브랜드를 선택하면 해당하는 선택한 브랜드의 itemList 페이지로 이동할 수 있다.", () => {
+    cy.get(".categoryBox", { timeout: 3000 }).first().click();
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq(`/itemlist/${ITEMLIST_ID}`);
     });
@@ -33,7 +33,7 @@ describe("E2E Test", () => {
   it("사용자는 상품들 리스트에서 상품을 클릭해서 상품 상세페이지를 볼 수 있다..", () => {
     cy.get(".itemListBox").first().click();
     cy.location().should((loc) => {
-      expect(loc.pathname).to.eq(`/items/${ITEM_ID}`);
+      expect(loc.pathname).to.eq(`/item/${ITEM_ID}`);
     });
   });
 
